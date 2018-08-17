b5f4209f0a3ec8861c83fd243f0d74afe1944443
Kshitiz Dublish <kshitiz.dublish@phasezeroventures.com>
2018-08-17 13:29:09 +0530

    Add Update script

---
+++ playground/mongo-update.js
@@ -0,0 +1,39 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    var db = client.db('TodoApp');
+
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID('5b76789c05a3330d660ced16')
+    // }, {
+    //     $set: {
+    //         completed: false
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+        _id: 122
+    }, {
+        $set: {
+            name: 'Kshitiz'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+});
